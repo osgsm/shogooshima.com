1ba6f21fd43147d2dc784a67e69263127f033552
Shogo Oshima <50955473+oshimax@users.noreply.github.com>
2022-02-15 11:38:21 +0900

    Initial commit

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyle from '../../styles/utils.module.css';
+import { GetStaticProps, GetStaticPaths } from 'next';
+
+type PostProps = {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const Post = ({ postData }: PostProps) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyle.headingXl}>{postData.title}</h1>
+      </article>
+      <div className={utilStyle.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+};
+
+export default Post;
