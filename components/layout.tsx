cc1421dd3f1eee49f8d6675a9c8a42881bb9e53b
oshimax <50955473+oshimax@users.noreply.github.com>
2022-02-24 15:26:38 +0900

    Fix og:title

--- components/layout.tsx
+++ components/layout.tsx
@@ -27,7 +27,7 @@ const Layout = ({ children, home }: LayoutProps) => {
             siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={siteTitle} />
+        <meta property="og:title" content={siteTitle} key="title" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
